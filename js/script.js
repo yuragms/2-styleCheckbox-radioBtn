4020c210a285cf8042e2db59c26a47d527a5d597
yuragms <yura.gms@gmail.com>
2022-03-12 19:25:59 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -15,4 +15,24 @@ $(document).ready(function () {
 
     return false;
   });
+
+  //RADIO
+  $.each($(".radiobuttons__item"), function (index, val) {
+    if ($(this).find("input").prop("checked") == true) {
+      $(this).addClass("active");
+    }
+  });
+  $(document).on("click", ".radiobuttons__item", function (event) {
+    $(this)
+      .parents(".radiobuttons")
+      .find(".radiobuttons__item")
+      .removeClass("active");
+    $(this)
+      .parents(".radiobuttons")
+      .find(".radiobuttons__item input")
+      .prop("checked", false);
+    $(this).toggleClass("active");
+    $(this).find("input").prop("checked", true);
+    return false;
+  });
 });
